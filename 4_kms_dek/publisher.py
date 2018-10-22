8f8dbb3dd8b925bcfac3dd4e301ce72013cd4aa1
salmaan rashid <salrashid123@gmail.com>
2018-10-22 07:04:06 -0700

    fix example4

--- 4_kms_dek/publisher.py
+++ 4_kms_dek/publisher.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python
 
-# Copyright 2018 Google Inc. All rights reserved.
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-#  python publisher.py  --mode encrypt --service_account '../svc-publisher.json' --project_id esp-demo-197318 --pubsub_topic my-new-topic   --kms_location us-central1 --kms_key_ring_id mykeyring --kms_key_id key1
-#  --dek JCZRt+IeTnTFB9OH8McpTlRQYR3I3MHBLy29VhfIUn4=
-
-
-# python publisher.py  --mode sign --service_account '../svc-publisher.json' --project_id esp-demo-197318 --pubsub_topic my-new-topic   --kms_location us-central1 --kms_key_ring_id mykeyring --kms_key_id key1
-
 import os ,sys
 import time
 import logging
@@ -28,7 +7,7 @@ import logging
 from google.cloud import pubsub
 import argparse
 import jwt
-import json
+import json, time
 import base64, binascii
 import httplib2
 from apiclient.discovery import build
@@ -39,18 +18,18 @@ from expiringdict import ExpiringDict
 import utils
 from utils import AESCipher, RSACipher, HMACFunctions
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s')
 
 parser = argparse.ArgumentParser(description='Publish encrypted message with KMS only')
 parser.add_argument('--service_account',required=True,help='publisher service_acount credentials file')
 parser.add_argument('--mode',required=True, choices=['encrypt','sign'], help='mode must be encrypt or sign')
+parser.add_argument('--kms_project_id',required=True, help='publisher KMS project')
 parser.add_argument('--kms_location',required=True, help='KMS Location')
 parser.add_argument('--kms_key_ring_id',required=True, help='KMS key_ring_id')
 parser.add_argument('--kms_key_id',required=True, help='KMS keyid')
-parser.add_argument('--project_id',required=True, help='publisher projectID')
+parser.add_argument('--pubsub_project_id',required=True, help='publisher projectID')
 parser.add_argument('--pubsub_topic',required=True, help='pubsub_topic to publish message')
-parser.add_argument('--dek',required=False, help='Static DEK to use for message encryption')
 parser.add_argument('--tenantID',required=False, default="tenantKey", help='Optional additionalAuthenticatedData')
 args = parser.parse_args()
 
@@ -65,8 +44,8 @@ if credentials.create_scoped_required():
 http = httplib2.Http()
 credentials.authorize(http)
 
-project_id = args.project_id
-os.environ['GOOGLE_CLOUD_PROJECT'] = project_id
+pubsub_project_id = args.pubsub_project_id
+kms_project_id = args.kms_project_id
 location_id = args.kms_location
 key_ring_id = args.kms_key_ring_id
 crypto_key_id = args.kms_key_id
@@ -79,100 +58,101 @@ cache = ExpiringDict(max_len=100, max_age_seconds=20)
 
 kms_client = build('cloudkms', 'v1')
 name = 'projects/{}/locations/{}/keyRings/{}/cryptoKeys/{}'.format(
-        project_id, location_id, key_ring_id, crypto_key_id)
-
-cleartext_message = {
-        "data" : "foo".encode(),
-        "attributes" : {
-            'epoch_time':  int(time.time()),
-            'a': "aaa",
-            'c': "ccc",
-            'b': "bbb"
-        }
-}
-
+        kms_project_id, location_id, key_ring_id, crypto_key_id)
 
 if args.mode =="sign":
   logging.info(">>>>>>>>>>> Start Sign with with locally generated key. <<<<<<<<<<<")
 
+  logging.info("Rotating key")
   hh = HMACFunctions()
   sign_key = hh.getDerivedKey()
-  logging.info("Generated Derived Key: " + base64.b64encode(sign_key))
-  msg_hash = hh.hash(json.dumps(cleartext_message))
-  logging.info("Generated Signature: " + msg_hash)
-  logging.info("End signature")
+  logging.debug("Generated Derived Key: " + base64.b64encode(sign_key))
 
   logging.info("Starting KMS encryption API call")
   crypto_keys = kms_client.projects().locations().keyRings().cryptoKeys()
   request = crypto_keys.encrypt(
             name=name,
             body={
-             'plaintext': base64.b64encode(sign_key).decode('utf-8'),
-             'additionalAuthenticatedData': base64.b64encode(tenantID).decode('utf-8')
+            'plaintext': base64.b64encode(sign_key).decode('utf-8'),
+            'additionalAuthenticatedData': base64.b64encode(tenantID).decode('utf-8')
             })
   response = request.execute()
   sign_key_wrapped = response['ciphertext'].encode('utf-8')
   logging.info("End KMS encryption API call")
 
+  cleartext_message = {
+          "data" : "foo".encode(),
+          "attributes" : {
+              'epoch_time':  int(time.time()),
+              'a': "aaa",
+              'c': "ccc",
+              'b': "bbb"
+          }
+  }
+
+  msg_hash = hh.hash(json.dumps(cleartext_message))
+  logging.debug("Generated Signature: " + msg_hash)
+  logging.debug("End signature")
+
   logging.info("Start PubSub Publish")
   publisher = pubsub.PublisherClient()
   topic_name = 'projects/{project_id}/topics/{topic}'.format(
-    project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+    project_id=pubsub_project_id,
     topic=PUBSUB_TOPIC,
   )
 
   publisher.publish(topic_name, data=json.dumps(cleartext_message), kms_key=name, sign_key_wrapped=sign_key_wrapped, signature=msg_hash)
   logging.info("Published Message: " + str(cleartext_message))
   logging.info(" with key_id: " + name)
-  logging.info(" with wrapped signature key " + sign_key_wrapped)
-  logging.info("End PubSub Publish")
+  logging.debug(" with wrapped signature key " + sign_key_wrapped)
+
+  logging.debug("End PubSub Publish")
   logging.info(">>>>>>>>>>> END <<<<<<<<<<<")
 
 if args.mode =="encrypt":
     logging.info(">>>>>>>>>>> Start Encryption with locally generated key.  <<<<<<<<<<<")
-
-    if  tenantID in cache:
-      dek = cache[tenantID]
-      logging.info("Reusing existing dek: " + base64.b64encode(dek) )
-    else:
-      # 256bit AES key
-      dek = os.urandom(32)
-      logging.info("Generated dek: " + base64.b64encode(dek) )
-      cache[tenantID] = dek
-
-    logging.info("Starting AES encryption")
-    ac = AESCipher(dek)
-
-    encrypted_message = ac.encrypt(json.dumps(cleartext_message))
-    logging.info("End AES encryption")
-    logging.info("Encrypted Message with dek: " + encrypted_message)
-
+    logging.info("Rotating symmetric key")
+    # 256bit AES key
+    dek = os.urandom(32)
+    logging.debug("Generated dek: " + base64.b64encode(dek) )
 
     logging.info("Starting KMS encryption API call")
     crypto_keys = kms_client.projects().locations().keyRings().cryptoKeys()
     request = crypto_keys.encrypt(
             name=name,
             body={
-             'plaintext': base64.b64encode(dek).decode('utf-8'),
-             'additionalAuthenticatedData': base64.b64encode(tenantID).decode('utf-8')
+            'plaintext': base64.b64encode(dek).decode('utf-8'),
+            'additionalAuthenticatedData': base64.b64encode(tenantID).decode('utf-8')
             })
     response = request.execute()
     dek_encrypted = response['ciphertext'].encode('utf-8')
-    #request = crypto_keys.decrypt(
-    #        name=name,
-    #        body={
-    #         'ciphertext': (dek_encrypted).decode('utf-8'),
-    #         'additionalAuthenticatedData': base64.b64encode(additionalAuthenticatedData).decode('utf-8')
-    #        })
-    #response = request.execute()
-    #logging.info(base64.b64decode(response['plaintext'].encode('utf-8')))
-    logging.info("Wrapped dek: " + dek_encrypted)
+
+    logging.debug("Wrapped dek: " + dek_encrypted)
     logging.info("End KMS encryption API call")
+
+    logging.debug("Starting AES encryption")
+    ac = AESCipher(dek)
+
+        
+    cleartext_message = {
+            "data" : "foo".encode(),
+            "attributes" : {
+                'epoch_time':  int(time.time()),
+                'a': "aaa",
+                'c': "ccc",
+                'b': "bbb"
+            }
+    }
+
+    encrypted_message = ac.encrypt(json.dumps(cleartext_message))
+    logging.debug("End AES encryption")
+    logging.debug("Encrypted Message with dek: " + encrypted_message)
+
 
     logging.info("Start PubSub Publish")
     publisher = pubsub.PublisherClient()
     topic_name = 'projects/{project_id}/topics/{topic}'.format(
-        project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+        project_id=pubsub_project_id,
         topic=PUBSUB_TOPIC,
     )
     publisher = pubsub.PublisherClient()
--- 4_kms_dek/subscriber.py
+++ 4_kms_dek/subscriber.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python
 
-# Copyright 2018 Google Inc. All rights reserved.
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# python subscriber.py  --mode decrypt --service_account '../svc-subscriber.json' --project_id esp-demo-197318 --pubsub_topic my-new-topic --pubsub_subscription my-new-subscriber --kms_location us-central1 --kms_key_ring_id mykeyring --kms_key_id key1
-
-# python subscriber.py  --mode verify --service_account '../svc-subscriber.json' --project_id esp-demo-197318 --pubsub_topic my-new-topic --pubsub_subscription my-new-subscriber --kms_location us-central1 --kms_key_ring_id mykeyring --kms_key_id key1
-
 import os
 import time
 from google.cloud import pubsub
@@ -35,15 +17,16 @@ from expiringdict import ExpiringDict
 import logging
 
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s')
 
 parser = argparse.ArgumentParser(description='Publish encrypted message with KMS only')
 parser.add_argument('--mode',required=True, choices=['decrypt','verify'], help='mode must be decrypt or verify')
 parser.add_argument('--service_account',required=True,help='publisher service_acount credentials file')
-parser.add_argument('--project_id',required=True, help='publisher service_acount credentials file')
+parser.add_argument('--pubsub_project_id',required=True, help='subscriber PubSub project')
 parser.add_argument('--pubsub_topic',required=True, help='pubsub_topic to publish message')
 parser.add_argument('--pubsub_subscription',required=True, help='pubsub_subscription to pull message')
+
 parser.add_argument('--kms_location_id',required=True, help='KMS kms_location_id (eg, us-central1)')
 parser.add_argument('--kms_key_ring_id',required=True, help='KMS kms_key_ring_id (eg, mykeyring)')
 parser.add_argument('--kms_key_id',required=True, help='KMS kms_key_id (eg, key1)')
@@ -61,8 +44,8 @@ if credentials.create_scoped_required():
 http = httplib2.Http()
 credentials.authorize(http)
 
-project_id = args.project_id
-os.environ['GOOGLE_CLOUD_PROJECT'] = project_id
+pubsub_project_id = args.pubsub_project_id
+
 tenantID = args.tenantID
 
 PUBSUB_TOPIC = args.pubsub_topic
@@ -73,11 +56,11 @@ crypto_keys = kms_client.projects().locations().keyRings().cryptoKeys()
 
 subscriber = pubsub.SubscriberClient()
 topic_name = 'projects/{project_id}/topics/{topic}'.format(
-    project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+    project_id=pubsub_project_id,
     topic=PUBSUB_TOPIC,
 )
 subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
-    project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+    project_id=pubsub_project_id,
     sub=PUBSUB_SUBSCRIPTION,
 )
 
@@ -94,27 +77,32 @@ def callback(message):
 
       logging.info("********** Start PubsubMessage ")
       logging.info('Received message ID: {}'.format(message.message_id))
-      logging.info('Received message publish_time: {}'.format(message.publish_time))
-      logging.info('Received message attributes["kms_key"]: {}'.format(message.attributes['kms_key']))
-      logging.info('Received message attributes["sign_key_wrapped"]: {}'.format(message.attributes['sign_key_wrapped']))
-      logging.info('Received message attributes["signature"]: {}'.format(message.attributes['signature']))
+      logging.debug('Received message publish_time: {}'.format(message.publish_time))
+      logging.debug('Received message attributes["kms_key"]: {}'.format(message.attributes['kms_key']))
+      logging.debug('Received message attributes["sign_key_wrapped"]: {}'.format(message.attributes['sign_key_wrapped']))
+      logging.debug('Received message attributes["signature"]: {}'.format(message.attributes['signature']))
       signature = message.attributes['signature']
       name = message.attributes['kms_key']
       sign_key_wrapped = message.attributes['sign_key_wrapped']
 
-      logging.info("Starting KMS decryption API call")
-      request = crypto_keys.decrypt(
-            name=name,
-            body={
-             'ciphertext': (sign_key_wrapped).decode('utf-8'),
-             'additionalAuthenticatedData': base64.b64encode(tenantID).decode('utf-8')
-            })
-      response = request.execute()
-      unwrapped_key =  base64.b64decode(response['plaintext'])
-      logging.info("End KMS decryption API call")
-      logging.info("Verify message: " + message.data)
-      logging.info('  With HMAC: ' + signature)
-      logging.info('  With unwrapped key: ' + base64.b64encode(unwrapped_key))
+      try:
+         unwrapped_key = cache[sign_key_wrapped]
+      except KeyError:
+        logging.info("Starting KMS decryption API call")
+        request = crypto_keys.decrypt(
+              name=name,
+              body={
+              'ciphertext': (sign_key_wrapped).decode('utf-8'),
+              'additionalAuthenticatedData': base64.b64encode(tenantID).decode('utf-8')
+              })
+        response = request.execute()
+        unwrapped_key =  base64.b64decode(response['plaintext'])
+        logging.info("End KMS decryption API call")
+        logging.debug("Verify message: " + message.data)
+        logging.debug('  With HMAC: ' + signature)
+        logging.debug('  With unwrapped key: ' + base64.b64encode(unwrapped_key))      
+        cache[sign_key_wrapped] = unwrapped_key
+
       hh = HMACFunctions(base64.b64encode(unwrapped_key))
       sig = hh.hash(message.data)
 
@@ -124,37 +112,42 @@ def callback(message):
       else:
         logging.error("Unable to verify message")
         message.nack()
-      logging.info("********** End PubsubMessage ")
+      logging.debug("********** End PubsubMessage ")
 
 
   if (args.mode == "decrypt"):
       logging.info("********** Start PubsubMessage ")
       logging.info('Received message ID: {}'.format(message.message_id))
-      logging.info('Received message publish_time: {}'.format(message.publish_time))
-      logging.info('Received message attributes["kms_key"]: {}'.format(message.attributes['kms_key']))
-      logging.info('Received message attributes["dek_wrapped"]: {}'.format(message.attributes['dek_wrapped']))
+      logging.debug('Received message publish_time: {}'.format(message.publish_time))
+      logging.debug('Received message attributes["kms_key"]: {}'.format(message.attributes['kms_key']))
+      logging.debug('Received message attributes["dek_wrapped"]: {}'.format(message.attributes['dek_wrapped']))
       dek_wrapped = message.attributes['dek_wrapped']
       name = message.attributes['kms_key']
 
-      logging.info("Starting KMS decryption API call")
-      request = crypto_keys.decrypt(
-            name=name,
-            body={
-             'ciphertext': (dek_wrapped).decode('utf-8'),
-             'additionalAuthenticatedData': base64.b64encode(tenantID).decode('utf-8')
-            })
-      response = request.execute()
-      dek=  base64.b64decode(response['plaintext'])
-      logging.info("End KMS decryption API call")
-      logging.info('Received aes_encryption_key : {}'.format( base64.b64encode(dek)))
+      try:
+         dek = cache[dek_wrapped]
+      except KeyError:
+        logging.info("Starting KMS decryption API call")
+        request = crypto_keys.decrypt(
+              name=name,
+              body={
+              'ciphertext': (dek_wrapped).decode('utf-8'),
+              'additionalAuthenticatedData': base64.b64encode(tenantID).decode('utf-8')
+              })
+        response = request.execute()
+        dek=  base64.b64decode(response['plaintext'])        
+        logging.info("End KMS decryption API call")
+        logging.debug('Received aes_encryption_key : {}'.format( base64.b64encode(dek)))
+        
+        cache[dek_wrapped] = dek
 
-      logging.info("Starting AES decryption")
+      logging.debug("Starting AES decryption")
       ac = AESCipher(dek)
       decrypted_data = ac.decrypt(message.data)
-      logging.info("End AES decryption")
+      logging.debug("End AES decryption")
       logging.info('Decrypted data ' + decrypted_data)
       message.ack()
-      logging.info("ACK message")
+      logging.debug("ACK message")
       logging.info("********** End PubsubMessage ")
 
 subscriber.subscribe(subscription_name, callback=callback)
